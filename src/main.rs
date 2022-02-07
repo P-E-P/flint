67da1721f5118eb6df4be9217b0bce74ffd665c7
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-02-07 01:03:59 +0100

    feat (flint): Initial commit.
    Initial commit with a minimal kernel printing a message in the VGA text
    buffer.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static WELCOME_MESSAGE: &[u8] = b"Booting flint...";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in WELCOME_MESSAGE.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
