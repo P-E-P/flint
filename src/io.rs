4b8a34005a2521efe09a07f11baae6cafe8b4662
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-02-10 18:24:08 +0100

    refacto (io): Fix io assembly code.
    The new syntax requires in/out assembly instructions instead of the old
    inb/outb and inw/outw.

--- src/io.rs
+++ src/io.rs
@@ -1,30 +1,37 @@
 use core::arch::asm;
 
 pub unsafe fn outb(value: u8, port: u16) {
-    asm!("outb {v},{p:x}",
-        v = in(reg_byte) value,
-        p = in(reg) port);
+    asm!("out dx, al",
+    in("dx") port,
+    in("al") value,
+    options(nomem, nostack)
+    );
 }
 
 pub unsafe fn inb(port: u16) -> u8 {
     let result: u8;
-    asm!("inb {p:x},{v}",
-        v = out(reg_byte) result,
-        p = in(reg) port);
+    asm!("in al, dx",
+    in("dx") port,
+    out("al") result,
+    options(nomem, nostack)
+    );
     result
 }
 
 pub unsafe fn outw(value: u16, port: u16) {
-    asm!("outw {v:x},{p:x}",
-        v = in(reg) value,
-        p = in(reg) port);
+    asm!("out dx, ax",
+        in("dx") port,
+        in("ax") value,
+        options(nomem, nostack));
 }
 
 pub unsafe fn inw(port: u16) -> u16 {
     let result: u16;
-    asm!("inw {p:x},{v:x}",
-        v = out(reg) result,
-        p = in(reg) port);
+    asm!("in ax, dx",
+        in("dx") port,
+        out("ax") result,
+        options(nomem, nostack)
+    );
     result
 }
 
