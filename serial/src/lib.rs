fb2de4602331c19f1c6e89000f07086aa0a94e59
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-02-16 17:08:54 +0100

    doc (serial): Document the serial module a bit.

--- serial/src/lib.rs
+++ serial/src/lib.rs
@@ -16,13 +16,20 @@ mod registers;
 
 type ComPort = usize;
 
+/// UART IO port 1 address
 pub const COM1: ComPort = 0x3F8;
+/// UART IO port 2 address
 pub const COM2: ComPort = 0x2F8;
+/// UART IO port 3 address
 pub const COM3: ComPort = 0x3E8;
+/// UART IO port 4 address
 pub const COM4: ComPort = 0x2E8;
 
+/// An option containing the default serial to use for communication.
 static mut DEFAULT: Option<Serial> = None;
 
+/// Retrieve a mutable reference to the default serial, initialize it before
+/// returning it in case it was not set previously.
 pub fn get_default() -> &'static mut Serial {
     unsafe {
         if DEFAULT.is_none() {
@@ -32,11 +39,19 @@ pub fn get_default() -> &'static mut Serial {
     }
 }
 
+/// A structure representing an UART device accessible through a given IO port.
 pub struct Serial {
+    /// The UART port address
     com_port: ComPort,
 }
 
 impl Default for Serial {
+    /// Return the default UART configuration which is:
+    /// - Port [`COM1`]
+    /// - Baud rate of 38400
+    /// - 8 bits word length
+    /// - No parity
+    /// - Interrupt trigger level of 14
     fn default() -> Self {
         let result = Serial::new(COM1);
         result.set_baud_rate(38400);
@@ -62,24 +77,31 @@ impl Default for Serial {
 }
 
 impl Serial {
+    /// Create a new Serial structure from a COM port address.
+    ///
+    /// # Note
+    /// This constructor does not initialize anything nor configure the UART
+    /// in any way.
     pub fn new(com_port: ComPort) -> Self {
         Serial { com_port }
     }
 
+    /// Set the transfer speed of an UART by setting it's DLL and DLH registers.
     pub fn set_baud_rate(&self, baud_rate: usize) {
         let dlv = 115200 / baud_rate;
         self.divisor_latch_low_byte().write((dlv & 0xff) as u8);
         self.divisor_latch_high_byte().write((dlv >> 8) as u8);
     }
 
-    /// Determine whether the current serial port can be used to write data on
-    /// the serial bus.
+    /// Determine whether data can be written safely to the data buffer register
+    /// of the UART.
     fn can_write(&self) -> bool {
         let register = self.line_status_register().read();
         register.empty_data_holding_registers() && register.empty_transmitter_holding_register()
     }
 
-    /// Function to write a byte to a given serial bus.
+    /// Write a byte to the serial bus of a given Serial structure. Waits for
+    /// the UART to be ready before sending it.
     ///
     /// # Arguments
     ///
@@ -91,6 +113,12 @@ impl Serial {
         self.transmitter_holding_buffer().write(byte);
     }
 
+    /// Write a string to the data buffer of the [`Serial`]. This function will
+    /// wait for the UART to be ready before sending any byte.
+    ///
+    /// # Arguments
+    ///
+    /// * `s` - The string to write.
     pub fn write_string(&self, s: &str) {
         for byte in s.bytes() {
             match byte {
@@ -103,54 +131,54 @@ impl Serial {
         }
     }
 
-    /// Get a line status register handle from the serial port.
-    fn line_status_register(&self) -> LineStatusRegister {
+    /// Get a [`LineStatusRegister`] handle from the serial port.
+    pub fn line_status_register(&self) -> LineStatusRegister {
         LineStatusRegister::from(self.com_port)
     }
 
-    /// Get a line control register handle from the serial port.
-    fn line_control_register(&self) -> LineControlRegister {
+    /// Get a [`LineControlRegister`] handle from the serial port.
+    pub fn line_control_register(&self) -> LineControlRegister {
         LineControlRegister::from(self.com_port)
     }
 
-    /// Get a transmitter holding buffer handle from the serial port.
-    fn transmitter_holding_buffer(&self) -> TransmitterHoldingBuffer {
+    /// Get a [`TransmitterHoldingBuffer`] handle from the serial port.
+    pub fn transmitter_holding_buffer(&self) -> TransmitterHoldingBuffer {
         TransmitterHoldingBuffer::from(self.com_port)
     }
 
-    /// Get a receiver buffer handle from the serial port.
-    fn receiver_buffer(&self) -> ReceiverBuffer {
+    /// Get a [`ReceiverBuffer`] handle from the serial port.
+    pub fn receiver_buffer(&self) -> ReceiverBuffer {
         ReceiverBuffer::from(self.com_port)
     }
 
-    /// Get a dll handle from the serial port.
-    fn divisor_latch_low_byte(&self) -> DivisorLatchLowByte {
+    /// Get a [`DivisorLatchLowByte`] handle from the serial port.
+    pub fn divisor_latch_low_byte(&self) -> DivisorLatchLowByte {
         DivisorLatchLowByte {
             address: self.com_port as u16,
             lcr: self.line_control_register(),
         }
     }
 
-    /// Get a dlh handle from the serial port.
-    fn divisor_latch_high_byte(&self) -> DivisorLatchHighByte {
+    /// Get a [`DivisorLatchHighByte`] handle from the serial port.
+    pub fn divisor_latch_high_byte(&self) -> DivisorLatchHighByte {
         DivisorLatchHighByte {
             address: self.com_port as u16 + 1,
             lcr: self.line_control_register(),
         }
     }
 
-    /// Get a fifo control register handle from the serial port.
-    fn fifo_control_register(&self) -> FifoControlRegister {
+    /// Get a [`FifoControlRegister`] handle from the serial port.
+    pub fn fifo_control_register(&self) -> FifoControlRegister {
         FifoControlRegister::from(self.com_port)
     }
 
-    /// Get an interrupt enable register handle from the serial port.
-    fn interrupt_enable_register(&self) -> InterruptEnableRegister {
+    /// Get an [`InterruptEnableRegister`] handle from the serial port.
+    pub fn interrupt_enable_register(&self) -> InterruptEnableRegister {
         InterruptEnableRegister::from(self.com_port)
     }
 
-    /// Get an interrupt identification register handle from the serial port.
-    fn interrupt_identification_register(&self) -> InterruptIdentificationRegister {
+    /// Get an [`InterruptIdentificationRegister`] handle from the serial port.
+    pub fn interrupt_identification_register(&self) -> InterruptIdentificationRegister {
         InterruptIdentificationRegister::from(self.com_port)
     }
 }
