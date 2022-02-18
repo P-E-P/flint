7b5b25e12ae4bf449a421b6bb7630844b2d92d8c
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-02-18 12:24:26 +0100

    feat (tests): Add a test framework.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga;
 #[macro_use]
 mod klog;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -16,6 +34,9 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     klog::init().ok();
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
