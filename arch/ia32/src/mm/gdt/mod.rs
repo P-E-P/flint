1bf15cd6d90fcb491e1677085e43febb79d9b451
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-03-14 17:58:03 +0100

    refactor(gdt): Move gdt setup back to ia32e
    The gdt setup was specific to ia32e, hence the move.

--- arch/ia32/src/mm/gdt/mod.rs
+++ arch/ia32/src/mm/gdt/mod.rs
@@ -1,18 +1,15 @@
-use crate::descriptor::segment::{
-    DefaultOperationSize, DescriptorType, Granularity, PrivilegeLevel, SegmentDescriptor,
-    SegmentType,
-};
+use crate::descriptor::segment::SegmentDescriptor;
 use core::arch::asm;
 use core::ptr::addr_of;
 use core::{fmt, mem};
-use log::{debug, trace};
+use log::trace;
 
 const GDT_LEN: usize = 5;
 
-pub struct GlobalDescriptorTable([SegmentDescriptor; GDT_LEN]);
+pub struct GlobalDescriptorTable(pub [SegmentDescriptor; GDT_LEN]);
 
 impl GlobalDescriptorTable {
-    fn load(self) {
+    pub fn load(self) {
         trace!("Loading global descriptor table...");
 
         let gdtr = GlobalDescriptorTableRegister::new(
@@ -59,55 +56,6 @@ impl GlobalDescriptorTableRegister {
 }
 
 pub fn setup_gdt() {
-    trace!("Setting up gdt...");
-    let gdt = GlobalDescriptorTable([
-        // Null segment
-        SegmentDescriptor::default(),
-        // Kernel code
-        SegmentDescriptor::new(0, 0xFFFF)
-            .segment_type(SegmentType::Code {
-                accessed: false,
-                read: true,
-                conforming: false,
-            })
-            .descriptor_type(DescriptorType::CodeOrData)
-            .ia32e_mode(true)
-            .privilege_level(PrivilegeLevel::Kernel)
-            .granularity(Granularity::FourKByte),
-        // Kernel data
-        SegmentDescriptor::new(0, 0xFFFF)
-            .segment_type(SegmentType::Data {
-                accessed: false,
-                write: true,
-                expand_down: false,
-            })
-            .descriptor_type(DescriptorType::CodeOrData)
-            .privilege_level(PrivilegeLevel::Kernel)
-            .default_operation_size(DefaultOperationSize::Segment32Bits)
-            .granularity(Granularity::FourKByte),
-        // User code
-        SegmentDescriptor::new(0, 0xFFFF)
-            .segment_type(SegmentType::Code {
-                accessed: false,
-                read: true,
-                conforming: false,
-            })
-            .descriptor_type(DescriptorType::CodeOrData)
-            .ia32e_mode(true)
-            .privilege_level(PrivilegeLevel::Userland)
-            .granularity(Granularity::FourKByte),
-        // User data
-        SegmentDescriptor::new(0, 0xFFFF)
-            .segment_type(SegmentType::Data {
-                accessed: false,
-                write: true,
-                expand_down: false,
-            })
-            .descriptor_type(DescriptorType::CodeOrData)
-            .privilege_level(PrivilegeLevel::Userland)
-            .default_operation_size(DefaultOperationSize::Segment32Bits)
-            .granularity(Granularity::FourKByte),
-    ]);
-    debug!("GDT:\n{}", gdt);
-    gdt.load();
+    trace!("Setting up 32bits gdt...");
+    todo!("Implement 32 bit GDT");
 }
