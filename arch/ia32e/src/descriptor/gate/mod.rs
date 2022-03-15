4d306981a46d3b2dc7f280209d200f8a9faebb76
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-03-15 20:39:37 +0100

    fix(ia32e/gate): Fix display implementation
    The display implementation for the ia32e gate was only accounting for
    64bits. This new implementation displays the whole 128bits.

--- arch/ia32e/src/descriptor/gate/mod.rs
+++ arch/ia32e/src/descriptor/gate/mod.rs
@@ -6,6 +6,13 @@ pub struct Gate(u128);
 
 impl fmt::Display for Gate {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:08X?};{:08X?}", self.0 >> 32, self.0 & 0xffffffff)
+        write!(
+            f,
+            "{:08X?};{:08X?};{:08X?};{:08X?}",
+            self.0 >> 96,
+            self.0 >> 64 & 0xffffffff,
+            self.0 >> 32 & 0xffffffff,
+            self.0 & 0xffffffff
+        )
     }
 }
