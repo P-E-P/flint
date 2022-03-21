ffd69d2d9a8acd7ebd738e88788f7b992402b6ca
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-03-21 11:52:27 +0100

    doc(io): Add safety comment
    Add safety comments to register's trait read and write definition.

--- arch/src/io/register.rs
+++ arch/src/io/register.rs
@@ -6,11 +6,21 @@ pub trait Register {
 /// Implementation required for a readable register.
 pub trait ReadRegister: Register {
     /// Read a value from the register and return it.
+    ///
+    /// # Safety
+    ///
+    /// Reading from a register might be unsafe depending on the context, double
+    /// check your code.
     unsafe fn read(&self) -> Self::Value;
 }
 
 /// Implementation required for a writable register.
 pub trait WriteRegister: Register {
     /// Write a value to the register.
+    ///
+    /// # Safety
+    ///
+    /// Writing to a register might be unsafe depending on the context, double
+    /// check your code.
     unsafe fn write(&self, value: Self::Value);
 }
