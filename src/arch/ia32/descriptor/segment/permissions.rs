9e87ca43abd48238f47282032af5ccd4492d6cc6
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-06-23 15:56:58 +0200

    feat(ia32/segment): Implement permissions display
    Implement the display trait for struct Permissions.

--- src/arch/ia32/descriptor/segment/permissions.rs
+++ src/arch/ia32/descriptor/segment/permissions.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use super::{DescriptorType, SegmentType};
 use crate::arch::ia32::PrivilegeLevel;
 use bit_field::BitField;
@@ -5,24 +6,40 @@ use bit_field::BitField;
 #[derive(Default, Copy, Clone)]
 pub struct Permissions(u8);
 
+const SEGMENT_UPPER : usize = 3;
+const DESC_TYPE_OFFSET : usize = 4;
+const PRIV_LEVEL_LOWER : usize = 5;
+const PRIV_LEVEL_UPPER : usize = 6;
+const PRESENT_OFFSET : usize = 7;
+
 impl Permissions {
     pub fn segment_type(&mut self, seg_type: SegmentType) -> &mut Self {
-        self.0.set_bits(..4, seg_type.into());
+        self.0.set_bits(..=SEGMENT_UPPER, seg_type.into());
         self
     }
 
     pub fn descriptor_type(&mut self, desc_type: DescriptorType) -> &mut Self {
-        self.0.set_bit(4, desc_type.into());
+        self.0.set_bit(DESC_TYPE_OFFSET, desc_type.into());
         self
     }
 
     pub fn privilege_level(&mut self, level: PrivilegeLevel) -> &mut Self {
-        self.0.set_bits(5..7, level.into());
+        self.0.set_bits(PRIV_LEVEL_LOWER..=PRIV_LEVEL_UPPER, level.into());
         self
     }
 
     pub fn present(&mut self, present: bool) -> &mut Self {
-        self.0.set_bit(7, present);
+        self.0.set_bit(PRESENT_OFFSET, present);
         self
+    }
+}
+
+impl fmt::Display for Permissions {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Type: {}\nS: {}\nDPL: {}\nPresent {}",
+            self.0.get_bits(..=SEGMENT_UPPER),
+            self.0.get_bit(DESC_TYPE_OFFSET),
+            self.0.get_bits(PRIV_LEVEL_LOWER..=PRIV_LEVEL_UPPER),
+            self.0.get_bit(PRESENT_OFFSET))
     }
 }
