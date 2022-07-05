e804145a16ea9c916d0b332f41872a77668a8f3d
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-05 12:33:38 +0200

    refactor(segment): Add unit tests for config
    Add unit tests for ia32 segment descriptor configuration structure.

--- src/arch/ia32/descriptor/segment/configuration.rs
+++ src/arch/ia32/descriptor/segment/configuration.rs
@@ -57,3 +57,77 @@ impl fmt::Display for Configuration {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    fn default_values() {
+        let conf = Configuration::default();
+        assert_eq!(conf.0, 0);
+    }
+
+    #[test_case]
+    fn limit_bits() {
+        let limit = 12;
+        let conf = Configuration::default().limit(limit);
+        assert_eq!(conf.0.get_bits(0..4), limit);
+    }
+
+    #[test_case]
+    fn limit_getter() {
+        let limit = 0xf;
+        let conf = Configuration::default().limit(limit);
+        assert_eq!(conf.get_limit(), limit);
+    }
+
+    #[test_case]
+    fn available() {
+        let conf = Configuration::default().available(true);
+        assert_eq!(conf.0.get_bit(4), true);
+    }
+
+    #[test_case]
+    fn not_available() {
+        let conf = Configuration::default().available(false);
+        assert_eq!(conf.0.get_bit(4), false);
+    }
+
+    #[test_case]
+    fn ia32e_mode() {
+        let conf = Configuration::default().ia32e_mode(true);
+        assert_eq!(conf.0.get_bit(5), true);
+        // Intel 3.4.5/L (64-bit code segment) flag
+        // > If L-bit is set, then D-bit must be cleared.
+        assert_eq!(conf.0.get_bit(6), false);
+    }
+
+    #[test_case]
+    fn db_16bits() {
+        use DefaultOperationSize::Segment16Bits;
+        let conf = Configuration::default().default_operation_size(Segment16Bits);
+        assert_eq!(conf.0.get_bit(6), false);
+    }
+
+    #[test_case]
+    fn db_32bits() {
+        use DefaultOperationSize::Segment32Bits;
+        let conf = Configuration::default().default_operation_size(Segment32Bits);
+        assert_eq!(conf.0.get_bit(6), true);
+    }
+
+    #[test_case]
+    fn granularity_byte() {
+        use Granularity::Byte;
+        let conf = Configuration::default().granularity(Byte);
+        assert_eq!(conf.0.get_bit(7), false);
+    }
+
+    #[test_case]
+    fn granularity_4kbyte() {
+        use Granularity::FourKByte;
+        let conf = Configuration::default().granularity(FourKByte);
+        assert_eq!(conf.0.get_bit(7), true);
+    }
+}
