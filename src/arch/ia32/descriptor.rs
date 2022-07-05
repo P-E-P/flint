293047f8acf303a717fe710b74856d9c192ba426
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-05 14:04:03 +0200

    refactor(descriptor): Remove unused functions
    Split limit and split address functions are not in use anymore thanks to
    bitfields, hence the removal.

--- src/arch/ia32/descriptor.rs
+++ src/arch/ia32/descriptor.rs
@@ -16,20 +16,3 @@ impl From<Granularity> for bool {
         }
     }
 }
-
-/// Split a 32 bits address in three parts, first part containing
-/// bits from 31 to 24, second part the bits 23 to 16 and last part
-/// bits 15 to 0.
-pub fn split_address(address: u32) -> (u32, u32, u32) {
-    (
-        (address >> 24) & 0xff,
-        (address >> 16) & 0xff,
-        address & 0xffff,
-    )
-}
-
-/// Split a 32 bit limit in two parts, first part contains bits
-/// from 19 to 16, second part contains bits from 15 to 0.
-pub fn split_limit(limit: u32) -> (u32, u32) {
-    (limit >> 16, limit & 0xffff)
-}
--- src/arch/ia32e/descriptor.rs
+++ src/arch/ia32e/descriptor.rs
@@ -1,5 +1,4 @@
 pub use crate::arch::ia32::descriptor::segment::*;
-pub use crate::arch::ia32::descriptor::split_limit;
 
 pub mod gate;
 pub mod tss;
