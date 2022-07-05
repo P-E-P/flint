3e5a2a37d9b3d652327d2b1119855af1fe0dcef6
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-06 00:56:24 +0200

    refactor(interrupt): Change configuration offsets
    Change configuration's offset formats from global with prefixes/suffixes
    to a more organized structure with submodules.

--- src/arch/ia32/descriptor/gate/interrupt/configuration.rs
+++ src/arch/ia32/descriptor/gate/interrupt/configuration.rs
@@ -5,10 +5,16 @@ use core::fmt;
 #[derive(Copy, Clone)]
 pub struct Configuration(u16);
 
-const PRIV_LEVEL_LOWER: usize = 13;
-const PRIV_LEVEL_UPPER: usize = 14;
-const PRESENT_OFFSET: usize = 15;
-const SIZE_OFFSET: usize = 11;
+mod offset {
+    pub const SIZE: usize = 11;
+
+    pub mod privilege_level {
+        pub const LOWER: usize = 13;
+        pub const UPPER: usize = 14;
+    }
+
+    pub const PRESENT: usize = 15;
+}
 
 impl Default for Configuration {
     fn default() -> Self {
@@ -18,31 +24,36 @@ impl Default for Configuration {
 
 impl Configuration {
     pub fn size(self, size: GateSize) -> Self {
-        Self(*self.0.clone().set_bit(SIZE_OFFSET, size.into()))
+        use offset::SIZE;
+        Self(*self.0.clone().set_bit(SIZE, size.into()))
     }
 
     pub fn privilege_level(self, level: PrivilegeLevel) -> Self {
+        use offset::privilege_level::{LOWER, UPPER};
         Self(
             *self
                 .0
                 .clone()
-                .set_bits(PRIV_LEVEL_LOWER..=PRIV_LEVEL_UPPER, u8::from(level).into()),
+                .set_bits(LOWER..=UPPER, u8::from(level).into()),
         )
     }
 
     pub fn present(self, present: bool) -> Self {
-        Self(*self.0.clone().set_bit(PRESENT_OFFSET, present))
+        use offset::PRESENT;
+        Self(*self.0.clone().set_bit(PRESENT, present))
     }
 }
 
 impl fmt::Display for Configuration {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use offset::{privilege_level, PRESENT, SIZE};
         write!(
             f,
             "Size: {}\nDPL: {}\nPresent {}",
-            self.0.get_bit(SIZE_OFFSET),
-            self.0.get_bits(PRIV_LEVEL_LOWER..=PRIV_LEVEL_UPPER),
-            self.0.get_bit(PRESENT_OFFSET)
+            self.0.get_bit(SIZE),
+            self.0
+                .get_bits(privilege_level::LOWER..=privilege_level::UPPER),
+            self.0.get_bit(PRESENT)
         )
     }
 }
