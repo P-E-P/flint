c2ea4c2bab4be8c87062695673aeb0979e872e4c
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-06 00:17:41 +0200

    doc(ia32/granularity): Document granularity
    Document granularity enum members.

--- src/arch/ia32/descriptor.rs
+++ src/arch/ia32/descriptor.rs
@@ -1,10 +1,14 @@
+//! A module containing the different descriptors structures.
 pub mod gate;
 pub mod segment;
 pub mod tss;
 
+/// The scaling of a descriptor's limit field.
 #[repr(u8)]
 pub enum Granularity {
+    /// Byte unit granularity.
     Byte = 0,
+    /// 4-KByte unit granularity.
     FourKByte = 1,
 }
 
