cbb47403d0cf7d2f15cbdb8d74302f5cf022976d
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-05 02:52:36 +0200

    refactor(macros): Remove now unused macros
    Offset and setbit macro are not used anymore since we do not rely on
    const flags to set values.

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,6 @@
 #![test_runner(crate::test::runner)]
 #![reexport_test_harness_main = "test_main"]
 
-/// Offset the bits of the given identifier by it's offset.
-#[macro_export]
-macro_rules! offset {
-    ($n: ident) => {
-        bits::$n << offsets::$n
-    };
-}
-
-/// Clear the flag bits `n` from a number `s` and set those bits to the new
-/// `v` value.
-#[macro_export]
-macro_rules! setbits {
-    ($s: expr, $v: expr, $n: ident) => {
-        // - Ensure the value does not overflow
-        // - Clear previous flag
-        // - Set new flag
-        ($s & !flags::$n) | (($v & bits::$n) << offsets::$n)
-    };
-}
-
 mod interrupts;
 mod mm;
 pub mod vga;
