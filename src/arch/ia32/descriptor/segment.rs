0cef3e9b42d4ddc952d946f4e866c333ec764c78
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-05 14:01:58 +0200

    doc(ia32/segment): Document SegmentDescriptor
    Document the SegmentDescriptor structure and it's methods.

--- src/arch/ia32/descriptor/segment.rs
+++ src/arch/ia32/descriptor/segment.rs
@@ -1,3 +1,6 @@
+//! A module containing the implementation, tests and revolving types around
+//! the [`SegmentDescriptor`] structure to describe the permissions and
+//! capacities of a memory segment.
 pub use super::Granularity;
 pub use crate::arch::ia32::PrivilegeLevel;
 use bit_field::BitField;
@@ -8,9 +11,17 @@ use permissions::Permissions;
 mod configuration;
 mod permissions;
 
+/// The default operation size modes of a memory segment.
+///
+/// It could be either 16 bits (`0`) or 32 bits (`0`). It performs different
+/// functions depending on wheter the segment descriptor is an executable code
+/// segment, an expand down data segment or a stack segment (cf. Intel volume
+/// III, 3.4.5 D/B (default operation flag))
 #[repr(u8)]
 pub enum DefaultOperationSize {
+    /// 16 bits memory segment default operation size.
     Segment16Bits = 0,
+    /// 32 bits memory segment default operation size.
     Segment32Bits = 1,
 }
 
@@ -41,12 +52,14 @@ impl From<DescriptorType> for bool {
 pub enum SegmentType {
     /// Data segment representation.
     Data {
+        /// Whether the segment has been accessed by the processor.
         accessed: bool,
         write: bool,
         expand_down: bool,
     },
     /// Code segment representation.
     Code {
+        /// Whether the segment has been accessed by the processor.
         accessed: bool,
         read: bool,
         conforming: bool,
@@ -71,7 +84,7 @@ impl From<SegmentType> for u8 {
 }
 
 /// A segment descriptor structure that can be used directly by the
-/// processor.
+/// processor to describe a memory segment.
 #[must_use]
 #[derive(Default, Copy, Clone)]
 #[repr(C, packed)]
@@ -87,6 +100,15 @@ pub struct SegmentDescriptor {
 impl SegmentDescriptor {
     /// Create a new [`SegmentDescriptor`] from an address and a limit
     /// with all other flags set to their default value.
+    ///
+    /// # Arguments
+    ///
+    /// * `base` - The segments base adress.
+    /// * `limit` - The limit value for the segment descriptor.
+    ///
+    /// # Note
+    ///
+    /// The present bit will be enabled when using this constructor.
     pub fn new(base: u32, limit: u32) -> Self {
         SegmentDescriptor {
             limit_15_0: limit.get_bits(..16).try_into().unwrap(),
@@ -99,7 +121,12 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Change the type of the segment by another [`SegmentType`].
+    /// Change a [`SegmentDescriptor`]'s segment type by another
+    /// [`SegmentType`].
+    ///
+    /// # Arguments
+    ///
+    /// * `seg_type` - The desired segment type.
     pub fn segment_type(self, seg_type: SegmentType) -> Self {
         Self {
             permissions: self.permissions.segment_type(seg_type),
@@ -107,7 +134,11 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Change the descriptor type by another [`DescriptorType`].
+    /// Change a [`SegmentDescriptor`]'s [`DescriptorType`] by another one.
+    ///
+    /// # Arguments
+    ///
+    /// * `desc_type` - The desired descriptor type.
     pub fn descriptor_type(self, desc_type: DescriptorType) -> Self {
         Self {
             permissions: self.permissions.descriptor_type(desc_type),
@@ -115,7 +146,11 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Set the privilege level of the segment.
+    /// Change a [`SegmentDescriptor`]'s privilege level by another one.
+    ///
+    /// # Arguments
+    ///
+    /// * `privilege_level` - The desired [`PrivilegeLevel`].
     pub fn privilege_level(self, level: PrivilegeLevel) -> Self {
         Self {
             permissions: self.permissions.privilege_level(level),
@@ -123,7 +158,11 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Set or clear the presence bit of the segment.
+    /// Change a [`SegmentDescriptor`]'s presence bit value.
+    ///
+    /// # Arguments
+    ///
+    /// * `present` - The desired bit value. Use `true` for 1 and `false` for 0.
     pub fn present(self, present: bool) -> Self {
         Self {
             permissions: self.permissions.present(present),
@@ -132,6 +171,10 @@ impl SegmentDescriptor {
     }
 
     /// Set or clear the available bit of the [`SegmentDescriptor`].
+    ///
+    /// # Arguments
+    ///
+    /// * `avl` - The desired bit value. Use `true` for 1 and `false` for 0.
     pub fn available(self, avl: bool) -> Self {
         Self {
             configuration: self.configuration.available(avl),
@@ -139,8 +182,18 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Set or clear the 64-bit code segment flag. If the bit is set, also
-    /// clear the D flag.
+    /// Change a [`SegmentDescriptor`]'s current mode to 32 bits or 64 bits.
+    ///
+    /// # Arguments
+    ///
+    /// * `mode` - The desired mode, `true` for 64 bits and `false` for 32 bits.
+    ///
+    /// # Note
+    ///
+    /// This method will overwrite any previous call to
+    /// [default_operation_size](SegmentDescriptor#method.default_operation_size)
+    /// if the mode is set to 64bits as it required the bit `D/B` set to `0`
+    /// (cf. Intel Volume III 3.4.5).
     pub fn ia32e_mode(self, mode: bool) -> Self {
         Self {
             configuration: self.configuration.ia32e_mode(mode),
@@ -148,7 +201,11 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Set the default operation size of the segment.
+    /// Change a [`SegmentDescriptor`]'s default operation size.
+    ///
+    /// # Arguments
+    ///
+    /// * `size` - The desired mode operation size.
     pub fn default_operation_size(self, size: DefaultOperationSize) -> Self {
         Self {
             configuration: self.configuration.default_operation_size(size),
@@ -156,7 +213,11 @@ impl SegmentDescriptor {
         }
     }
 
-    /// Set the granularity of the segment.
+    /// Change a [`SegmentDescriptor`]'s granularity.
+    ///
+    /// # Arguments
+    ///
+    /// * `granularity` - The desired granularity.
     pub fn granularity(self, granularity: Granularity) -> Self {
         Self {
             configuration: self.configuration.granularity(granularity),
@@ -164,12 +225,16 @@ impl SegmentDescriptor {
         }
     }
 
+    /// Get the whole reassembled base address from a [`SegmentDescriptor`]
+    /// fields.
     fn get_address(&self) -> u32 {
         u32::from(self.base_31_24) << 24
             | u32::from(self.base_23_16) << 16
             | u32::from(self.base_15_0)
     }
 
+    /// Get the whole reassembled segment limit from a [`SegmentDescriptor`]
+    /// fields.
     fn get_limit(&self) -> u32 {
         u32::from(self.configuration.get_limit()) << 16 | u32::from(self.limit_15_0)
     }
