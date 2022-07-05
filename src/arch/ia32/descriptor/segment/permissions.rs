6af328df57b468f54abe77475dd3495b2cb5e019
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-05 18:40:15 +0200

    doc(segment/permissions): Add documentation
    Add documentation to segments permission field.

--- src/arch/ia32/descriptor/segment/permissions.rs
+++ src/arch/ia32/descriptor/segment/permissions.rs
@@ -1,8 +1,18 @@
+//! A module containing the implementation and tests for the [`Permissions`]
+//! structure.
 use super::{DescriptorType, SegmentType};
 use crate::arch::ia32::PrivilegeLevel;
 use bit_field::BitField;
 use core::fmt;
 
+/// A structure representing the bits 8 to 15 from a
+/// [SegmentDescriptor](super::SegmentDescriptor) structure.
+///
+/// It gathers the following fields:
+/// - Segment type (TYPE)
+/// - Descriptor type (S)
+/// - Descriptor privilege level (DPL)
+/// - Present bit (P)
 #[derive(Default, Copy, Clone)]
 pub struct Permissions(u8);
 
@@ -35,21 +45,42 @@ mod offset {
 }
 
 impl Permissions {
+    /// Change a [`Permissions`]'s segment type value.
+    ///
+    /// # Arguments
+    ///
+    /// * `seg_type` - The desired [`SegmentType`] value.
     pub fn segment_type(self, seg_type: SegmentType) -> Self {
         use offset::segment_type::UPPER;
         Self(*self.0.clone().set_bits(..=UPPER, seg_type.into()))
     }
 
+    /// Change a [`Permissions`]'s descriptor type value.
+    ///
+    /// # Arguments
+    ///
+    /// * `desc_type` - The desired [`DescriptorType`] value.
     pub fn descriptor_type(self, desc_type: DescriptorType) -> Self {
         use offset::DESC_TYPE;
         Self(*self.0.clone().set_bit(DESC_TYPE, desc_type.into()))
     }
 
+    /// Change a [`Permissions`]'s privilege level by another one.
+    ///
+    /// # Arguments
+    ///
+    /// * `level` - The desired [`PrivilegeLevel`] value.
     pub fn privilege_level(self, level: PrivilegeLevel) -> Self {
         use offset::privilege_level::{LOWER, UPPER};
         Self(*self.0.clone().set_bits(LOWER..=UPPER, level.into()))
     }
 
+    /// Change a [`Permissions`]'s present bit.
+    ///
+    /// # Arguments
+    ///
+    /// * `present` - The desired bit value, `true` for bit value 1 and `false`
+    /// for bit value 0.
     pub fn present(self, present: bool) -> Self {
         use offset::PRESENT;
         Self(*self.0.clone().set_bit(PRESENT, present))
