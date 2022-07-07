b3d7e51cbcf81b4fcf0f09a485b0479d9317c228
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-07 23:07:54 +0200

    refactor(ia32/tss): Refactor permissions
    Add documentation and unit tests to Permissions structure of the TSS
    descriptor.

--- src/arch/ia32/descriptor/tss/permissions.rs
+++ src/arch/ia32/descriptor/tss/permissions.rs
@@ -1,13 +1,39 @@
+//! A module containing the implementation and tests for the [`Permissions`]
+//! structure.
 use crate::arch::ia32::PrivilegeLevel;
 use bit_field::BitField;
 use core::fmt;
 
+/// A structure representing the bits 8 to 15 from a
+/// [TssDescriptor](super::TssDescriptor) structure.
+///
+/// It gathers the following fields:
+/// - Segment type (TYPE)
+/// - Descriptor privilege level (DPL)
+/// - Present bit (P)
 #[derive(Copy, Clone)]
 pub struct Permissions(u8);
 
-const PRIV_LEVEL_LOWER: usize = 5;
-const PRIV_LEVEL_UPPER: usize = 6;
-const PRESENT_OFFSET: usize = 7;
+/// The set of all field offsets for the [`Permissions`] structure.
+mod offset {
+    /// Offset of the busy bit (B) within the [Permissions](super::Permissions)
+    /// structure
+    pub const BUSY: usize = 1;
+
+    /// Bounds of the descriptor privilege level (DPL) bits within the
+    /// [Permissions](super::Permissions) structure.
+    pub mod privilege_level {
+        /// Lower bit offset.
+        pub const LOWER: usize = 5;
+
+        /// Upper bit offset.
+        pub const UPPER: usize = 6;
+    }
+
+    /// Offset of the present bit (P) within the
+    /// [Permissions](super::Permissions) structure.
+    pub const PRESENT: usize = 7;
+}
 
 impl Default for Permissions {
     fn default() -> Self {
@@ -16,32 +42,103 @@ impl Default for Permissions {
 }
 
 impl Permissions {
+    /// Change a [`Permissions`]'s busy bit.
+    ///
+    /// # Arguments
+    ///
+    /// * `busy` - The desired bit value, `true` for bit value 1 and `false`
+    /// for bit value 0.
     pub fn busy(self, busy: bool) -> Self {
-        Self(*self.0.clone().set_bit(1, busy))
+        use offset::BUSY;
+        Self(*self.0.clone().set_bit(BUSY, busy))
     }
 
+    /// Change a [`Permissions`]'s privilege level by another one.
+    ///
+    /// # Arguments
+    ///
+    /// * `level` - The desired [`PrivilegeLevel`] value.
     pub fn privilege_level(self, level: PrivilegeLevel) -> Self {
+        use offset::privilege_level::{LOWER, UPPER};
         Self(
             *self
                 .0
                 .clone()
-                .set_bits(PRIV_LEVEL_LOWER..=PRIV_LEVEL_UPPER, level.into()),
+                .set_bits(LOWER..=UPPER, level.into()),
         )
     }
 
+    /// Change a [`Permissions`]'s present bit.
+    ///
+    /// # Arguments
+    ///
+    /// * `present` - The desired bit value, `true` for bit value 1 and `false`
+    /// for bit value 0.
     pub fn present(self, present: bool) -> Self {
-        Self(*self.0.clone().set_bit(PRESENT_OFFSET, present))
+        use offset::PRESENT;
+        Self(*self.0.clone().set_bit(PRESENT, present))
     }
 }
 
 impl fmt::Display for Permissions {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use offset::{privilege_level, BUSY, PRESENT};
         write!(
             f,
             "Busy: {}\nDPL: {}\nPresent {}",
-            self.0.get_bit(1),
-            self.0.get_bits(PRIV_LEVEL_LOWER..=PRIV_LEVEL_UPPER),
-            self.0.get_bit(PRESENT_OFFSET)
+            self.0.get_bit(BUSY),
+            self.0.get_bits(privilege_level::LOWER..=privilege_level::UPPER),
+            self.0.get_bit(PRESENT)
         )
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    fn structure_size() {
+        use core::mem::size_of;
+        assert_eq!(size_of::<Permissions>(), 1);
+    }
+
+    #[test_case]
+    fn default_values() {
+        let perm = Permissions::default();
+        assert_eq!(perm.0.get_bits(..5), 0b01001);
+    }
+
+    #[test_case]
+    fn present() {
+        let perm = Permissions::default().present(true);
+        assert_eq!(perm.0.get_bit(7), true);
+    }
+
+    #[test_case]
+    fn privilege_kernel() {
+        use PrivilegeLevel::Kernel;
+        let perm = Permissions::default().privilege_level(Kernel);
+        assert_eq!(perm.0.get_bits(5..=6), 0);
+    }
+
+    #[test_case]
+    fn privilege_userland() {
+        use PrivilegeLevel::Userland;
+        let perm = Permissions::default().privilege_level(Userland);
+        assert_eq!(perm.0.get_bits(5..=6), 3);
+    }
+
+    #[test_case]
+    fn busy() {
+        let perm = Permissions::default().busy(true);
+        assert_eq!(perm.0.get_bit(1), true);
+    }
+
+    #[test_case]
+    fn not_busy() {
+        let perm = Permissions::default().busy(false);
+        assert_eq!(perm.0.get_bit(1), false);
+    }
+
 }
