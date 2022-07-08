fe6ea60321d95c362517d49dbce437f6afe3b3e8
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-08 13:31:44 +0200

    doc(ia32e/gate): Document structure and methods
    Document the Gate descriptor for ia32e.

--- src/arch/ia32e/descriptor/gate.rs
+++ src/arch/ia32e/descriptor/gate.rs
@@ -1,3 +1,4 @@
+//! A module containing the implementation and tests for the Gate structure.
 use crate::arch::ia32e::{selector::SegmentSelector, PrivilegeLevel};
 use bit_field::BitField;
 use configuration::Configuration;
@@ -5,9 +6,12 @@ use core::fmt;
 
 mod configuration;
 
+/// Type to determine whether a [`Gate`] is a trap gate or an interrupt gate.
 #[repr(u16)]
 pub enum Kind {
+    /// Interrupt gate type.
     Interrupt = 0xe,
+    /// Trap gate type.
     Trap = 0xf,
 }
 
@@ -17,21 +21,38 @@ impl From<Kind> for u16 {
     }
 }
 
+/// A gate descriptor structure that can be used to describe either a trap gate
+/// or an interrupt gate. This structure can be used directly by the processor.
 #[must_use]
 #[derive(Default, Copy, Clone)]
 #[repr(C, packed)]
 pub struct Gate {
+    /// Bits 0 to 15 of the interrupt routine address.
     offset_15_0: u16,
+    /// The segment selector to use on interrupt/trap.
     segment_selector: SegmentSelector,
+    /// The configuration of the gate descriptor.
     configuration: Configuration,
+    /// Bits 16 to 31 of the interrupt routine address.
     offset_31_16: u16,
+    /// Bits 32 to 63 of the interrupt routine address.
     offset_63_32: u32,
+    /// Reserved bits.
     _reserved: u32,
 }
 
 impl Gate {
     /// Creates a new interrupt/trap [`Gate`] from a given offset and segment
     /// selector.
+    ///
+    /// # Arguments
+    ///
+    /// * `base` - The segments base adress.
+    /// * `limit` - The limit value for the segment descriptor.
+    ///
+    /// # Note
+    ///
+    /// The present bit will be enabled when using this constructor.
     pub fn new(offset: u64, segment_selector: SegmentSelector) -> Self {
         Self {
             offset_15_0: offset.get_bits(0..16).try_into().unwrap(),
@@ -44,6 +65,10 @@ impl Gate {
     }
 
     /// Set or clear the presence bit of the [`Gate`].
+    ///
+    /// # Arguments
+    ///
+    /// * `present` - The desired bit value. Use `true` for 1 and `false` for 0.
     pub fn present(self, value: bool) -> Self {
         Self {
             configuration: self.configuration.present(value.into()),
@@ -52,6 +77,10 @@ impl Gate {
     }
 
     /// Set the type of [`Gate`].
+    ///
+    /// # Arguments
+    ///
+    /// * `kind` - The desired type of gate.
     pub fn kind(self, kind: Kind) -> Self {
         Self {
             configuration: self.configuration.kind(kind),
@@ -60,6 +89,10 @@ impl Gate {
     }
 
     /// Set the privilege level of the [`Gate`] descriptor.
+    ///
+    /// # Arguments
+    ///
+    /// * `privilege_level` - The desired [`PrivilegeLevel`].
     pub fn privilege_level(self, level: PrivilegeLevel) -> Self {
         Self {
             configuration: self.configuration.privilege_level(level),
@@ -68,6 +101,15 @@ impl Gate {
     }
 
     /// Set the  interrupt stack table.
+    ///
+    /// # Arguments
+    ///
+    /// * `ist` - The desired interrupt stack table.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the ist value is too high and cannot fit
+    /// the boundaries of the ist field.
     pub fn interrupt_stack_table(self, ist: u8) -> Self {
         Self {
             configuration: self.configuration.interrupt_stack_table(ist),
